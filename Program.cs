4480b9ebb15547b3b07ee6618f2c6ced5ef165be
inkoz <inkoz@DESKTOP-JS5JFS1>
2022-07-06 13:55:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+
+       static void Cube(int[] cube)
+        {
+            int counter = 0;
+            int length = cube.Length;
+            while (counter < length)
+            {
+                cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+                counter++;
+            }
+        }
+
+        static void PrintArry(int[] coll)
+        {
+            int count = coll.Length;
+            int index = 0;
+            while (index < count)
+            {
+                Console.Write(coll[index] + " ");
+                index++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+            Console.Write("Введите число: ");
+            int cube = Convert.ToInt32(Console.ReadLine());
+
+           
+
+            int[] arry = new int[cube + 1];
+            Cube(arry);
+            PrintArry(arry);
+        }
+    }
+}
